7205ae1ab38164db3bc2faeddb44916a4e22d82a
ty850454 <ty850454@126.com>
2019-02-23 00:35:26 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,440 @@
+<template>
+  <div id="app">
+    <div class="fiex">
+      <div class="head">
+          <div class="logo">江苏倍鑫特实业发展有限公司LOGO</div>
+          <div class="menu">
+            <ul>
+              <li>
+                <router-link to="/"><img src="./assets/ico_1.png"><div>关于倍鑫特</div></router-link>
+              </li>
+              <li>
+                <router-link to="/"><img src="./assets/ico_2.png"><div>产品中心</div></router-link>
+              </li>
+              <li>
+                <router-link to="/"><img src="./assets/ico_6.png"><div>人才招聘</div></router-link>
+              </li>
+              <li>
+                <router-link to="/"><img src="./assets/ico_7.png"><div>联系我们</div></router-link>
+              </li>
+            </ul>
+          </div>
+
+      </div>
+    </div>
+    <div class="body">
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+占位<br>
+    </div>
+    <div class="foot">
+
+      <div class="item">
+        <h3><a href="acting_brand.aspx">产品中心</a></h3>
+        <ul>
+            <li><a href="acting_brand.aspx?cid=1">代理品牌</a></li>
+            <li><a href="advantage_brand.aspx?cid=2">优势品牌</a></li>
+            <li><a href="distribution_brand.aspx?cid=3">经销品牌</a></li>
+            <li><a href="pro_classification.aspx?cid=4">产品分类</a></li>
+            <li><a href="industry.aspx?cid=5">行业分类</a></li>
+          </ul>
+      </div>
+      <div class="item">
+        <h3><a href="acting_brand.aspx">产品中心</a></h3>
+        <ul>
+            <li><a href="acting_brand.aspx?cid=1">代理品牌</a></li>
+            <li><a href="advantage_brand.aspx?cid=2">优势品牌</a></li>
+            <li><a href="distribution_brand.aspx?cid=3">经销品牌</a></li>
+            <li><a href="pro_classification.aspx?cid=4">产品分类</a></li>
+            <li><a href="industry.aspx?cid=5">行业分类</a></li>
+          </ul>
+      </div>
+
+
+
+
+    </div>
+
+
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+
+.head {
+  margin: 0 auto;
+  max-width: 1080px;
+  height: 100px;
+  background-color: #fff;
+}
+
+
+.head .logo {
+  float: left;
+  line-height: 100px;
+}
+.head .menu {
+  float: right;
+  margin-top: 10px;
+  margin-right: 50px;
+}
+.head > .body::after {
+  content: ' ';
+  clear: both;
+}
+.fiex {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  border-bottom: 1px solid #ededee;
+}
+
+.menu > ul {
+  list-style: none;
+}
+.menu > ul li {
+  margin-left: 50px;
+  display: inline-block;
+}
+.menu > ul li img {
+  height: 22px;
+}
+.menu > ul li a {
+    color: #666;
+}
+.body {
+margin-top: 100px;
+}
+
+
+.foot {
+  display: flex;
+  justify-content: center;
+  background-color: #f0f0f0;
+  min-width: 1080px;
+  padding-bottom: 50px;
+}
+.foot > .item {
+  border-left: 1px solid #dcdcdc;
+  margin: 20px  5px;
+  padding: 0 20px;
+}
+.foot > .item > ul  {
+  list-style: none;
+  margin: 0;
+}
+.foot > .item > h3  {
+  margin: 0;
+}
+.foot > .item a  {
+  color: #777;
+}
+
+body {
+  margin: 0;
+}
+a {
+  text-decoration: none;
+}
+ul {
+  padding-left: 0;
+}
+</style>
